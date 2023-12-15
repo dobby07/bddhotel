161905be6dea30d21aa96212a979bcaaa12837b3
Pedro Alferez <pedalferez@upt.pe>
2023-12-14 23:17:43 -0500

    v3

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features", glue= {"StepDefinitions"},
 monochrome= true,
-plugin = {"pretty", "html:target/HtmlReports"}
+plugin = {"pretty", "html:target/cucumber/report.html", "json:target/cucumber/Report.json"}
 		)
 public class TestRunner {
 
